29e5c3ec10e9bf71fa35660b862b1d4a4dd45ab6
unknown <bardelinprojetos@gmail.com>
2023-02-23 01:05:09 -0300

    Correio Automacao

--- automacaoCorreios/Core/Begin.cs
+++ automacaoCorreios/Core/Begin.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+
 
 namespace automacaoCorreios.Core
 {
@@ -12,7 +14,7 @@ namespace automacaoCorreios.Core
         [SetUp]
         public void InicioTeste()
 		{
-            driver = new ChromeDriver(@"/Users/leonardobardelin/Projects/automacaoCorreios/automacaoCorreios/Driver");
+            driver = new ChromeDriver(@"C:\Users\Pichau\Desktop\Projetos\automacaoCorreios\Driver\");
             driver.Navigate().GoToUrl("https://www.correios.com.br/");
             driver.Manage().Window.Maximize();
         }
@@ -21,20 +23,20 @@ namespace automacaoCorreios.Core
         public void FimDoTeste()
         {
             var todayDate = DateTime.Now;
-            var localtime = todayDate.ToString("yy-MM-dd HH:mm:ss");
+            var localtime = todayDate.ToString("yy_MM_dd HH-mm-ss");
             var testName = TestContext.CurrentContext.Test.Name;
 
             if (TestContext.CurrentContext.Result.Outcome != ResultState.Success != true)
             {
                 var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-                var filePath = "/Users/leonardobardelin/Projects/Correios-Automacao/automacaoCorreios/ScreenshotReport/";
-                screenshot.SaveAsFile(filePath + "Sucesso - " + testName + ".jpeg " + localtime);
+                var filePath = @"C:\Users\Pichau\Desktop\Projetos\automacaoCorreios\ScreenshotReport\";
+                screenshot.SaveAsFile(filePath + @"\Sucesso - " + testName + ".jpeg " + localtime);
             }
                 else
                 {
                     var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
-                    var filePath = "/Users/leonardobardelin/Projects/Correios-Automacao/automacaoCorreios/ScreenshotReport/";
-                    screenshot.SaveAsFile(filePath + "Falha - " + testName + ".jpeg " + localtime);
+                    var filePath = @"C:\Users\Pichau\Desktop\Projetos\automacaoCorreios\ScreenshotReport\";
+                    screenshot.SaveAsFile(filePath + @"\Falha - " + testName + ".jpeg " + localtime);
                 }
 
             if (driverQuit) driver.Quit();
